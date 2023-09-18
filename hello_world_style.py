1118b401745a3bc0f6e15e1399e77efae3043d7f
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-09-18 16:14:19 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created By: Tony Tra.
+
+# Date: September 18, 2023
+
+# Prints Hello World with Style.
+
+
+def main():
+    print("Hello, World! With Style")
+
+
+if __name__ == "__main__":
+    main()
